e3423ef6f79967f67f79c625a2584e5037b4b832
Stavr101 <Shishkin_SU@ukr.net>
2023-01-13 00:11:26 +0200

    fin task6

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,9 +1,9 @@
 const bcrypt = require("bcrypt");
 const gravatar = require("gravatar");
-
+const { nanoid } = require("nanoid");
 const { User } = require("../../models/user");
 
-const { HttpError } = require("../../helpers");
+const { HttpError, sendEmail } = require("../../helpers");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -16,12 +16,20 @@ const register = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationToken = nanoid();
   const newUser = await User.create({
     ...req.body,
     password: hashPassword,
     avatarURL,
+    verificationToken,
   });
 
+  const mail = {
+    to: email,
+    subject: "Подтверждение регистрации на сайте",
+    html: `<a href="http://localhost:3000/api/v1/auth/verify/${verificationToken}" target="_blank">Нажмите для подтверждения регистрации</a>`,
+  };
+  await sendEmail(mail);
   res.status(201).json({
     name: newUser.name,
     email: newUser.email,
